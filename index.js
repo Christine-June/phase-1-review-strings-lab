b44303a4f08873112b84b7590ae3f90a894801f7
Christine-June <gathonichris12@gmail.com>
2025-03-06 08:57:02 +0300

    Works through tests

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Christine June';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting with only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
